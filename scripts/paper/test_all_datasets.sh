652a6d68542690aec5b9446a73aa316d6e744c33
KohlerHECTOR <hector.kohler@inria.fr>
2026-06-29 12:09:16 +0200

    stuff removed

--- scripts/paper/test_all_datasets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=graphtester
-#SBATCH --mem=250G
-#SBATCH --cpus-per-task=10
-
-# Run the test_all_datasets.py script
-source $(poetry env info --path)/bin/activate
-yes | python ./scripts/paper/test_all_datasets.py
--- tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Graphtester package tests."""
